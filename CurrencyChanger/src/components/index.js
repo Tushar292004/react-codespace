59ec98731648fc45eead80f3cbf4f20f0d8613bf
TUSHAR292004 <tusharchandak2004@gmail.com>
2024-04-05 22:01:48 +0530

    react-codespace

---
+++ CurrencyChanger/src/components/index.js
@@ -0,0 +1,3 @@
+//bring all the components here and then export it best use habits for big projects
+import InputBox from "./InputBox";
+export {InputBox}
--- CurrencyChanger/src/hooks/useCurrencyInfo.js
+++ CurrencyChanger/src/hooks/useCurrencyInfo.js
@@ -2,9 +2,11 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({}) // having an empty object in useState to  avoid errors if the fetch call in not coming.
+    const [data, setData] = useState({}) 
+    // having an empty object in useState to  avoid errors if the fetch call in not coming.
 
     useEffect(() => {
+        //fetching the api which gives the string data of all the currency information
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`) 
         //it will give a string response
         .then((res) => res.json()) 
@@ -12,12 +14,17 @@ function useCurrencyInfo(currency) {
         .then((res) => setData(res[currency]))
         // accessing the currency values from json response
 
-    }, [currency] //dependency array will automatically call the function if these fields are updated
+    }, [currency] 
+    //dependency array will automatically call the function if these fields are updated
     )
 
-    console.log(data);
-    return data
+    console.log(data); 
+    //checking the values
+
+    return data //
 }
 
 export default useCurrencyInfo; 
-//resturning the whole useCurrenctInfo method so that we will get value of  fetch api, data, setData.
+//returning the whole useCurrenctInfo method so that we will get value of  fetch api, data, setData.
+
+//custom hook is finally designed  
